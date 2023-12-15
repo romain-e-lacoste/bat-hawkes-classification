e272c5c7284fbbe4bb393cda9ff36e690beae0f7
Romain E. LACOSTE <143622132+romain-e-lacoste@users.noreply.github.com>
2023-12-15 18:50:53 +0100

    Update goodness_of_fit_test.py

--- functions/goodness_of_fit_test.py
+++ functions/goodness_of_fit_test.py
@@ -14,7 +14,6 @@ from scipy import stats
 
 
 #%% Functions 
-
 '''Function for computing the compensator of Hawkes process with exponential kernel.
 
    Input: - t : 'float', upper bound of the integration interval
@@ -25,9 +24,10 @@ from scipy import stats
             
    Output: - Lambda(t) : 'tensor', compensator of the Hawkes process
 '''
-
 def compensator(t, jump_times, alpha, mu, beta):
+   
     jump_times = jump_times[jump_times < t]
+   
     return ((mu.mul(t)).add(-alpha.mul(torch.sum(torch.exp(beta.mul(-t.add(-jump_times)))-1))))
 
 '''Function for performing the goodness-of-fit test. 
@@ -39,9 +39,11 @@ def compensator(t, jump_times, alpha, mu, beta):
             
    Output: - p-value : 'flaot', associated p-value given by the Kolmogorov-Smirnov test
 '''
-
 def goodness_of_fit_test(jump_times, alpha, mu, beta):
+   
     unit_poisson_jumptimes = torch.empty(len(jump_times))
+   
     for i in range (len(jump_times)):
         unit_poisson_jumptimes[i] = compensator(jump_times[i], jump_times, alpha, mu, beta)
+       
     return (stats.kstest(np.array(torch.diff(unit_poisson_jumptimes)), stats.expon.cdf)[1])
